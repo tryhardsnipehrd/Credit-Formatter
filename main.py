d6707d34c20a8e12c1ad31777416b66144388182
Tryhardsnipehrd <tryhrdsnphrd@tryhrdsnphrd.com>
2023-12-10 18:52:58 -0700

    Add README

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import requests
 import configparser
 
+
+# Setup the config parser, and read the needed values from the file
 config = configparser.ConfigParser()
 config.read('config.ini')
 CreatorToken = config['Patreon']['CreatorToken']
 CampaignID = config['Patreon']['CampaignID']
 SaveFile = config['General']['SaveFile']
 
+# Initialize a global dictionary to use for lookup from ID
 all_tiers = {}
 
 
@@ -17,10 +20,13 @@ def get_patrons():
 
 
 def setup_tiers():
+    # This URL is used to get only the tiers, but also includes patrons as well.
+    # No identifying information is included, except for a unique ID for each member.
     access_url = f"https://www.patreon.com/api/oauth2/v2/campaigns/{CampaignID}/members?include=currently_entitled_tiers&fields[tier]=title"
+    # Make the request to the API
     PatreonResponse = requests.get(access_url, headers={"Authorization": f"Bearer {CreatorToken}"})
-    # print(PatreonResponse.json())
 
+    # Populate the global dictionary with the tier ID as the key, and the tier name as the value
     for tier in PatreonResponse.json()['included']:
         all_tiers[tier['id']] = tier['attributes']['title']
 
@@ -39,6 +45,7 @@ if __name__ == '__main__':
     # print(all_patrons)
     print("Creating list...")
     with open(SaveFile, 'a') as f:
+        # Loop through the patrons, writing the data to the savefile
         for patron in all_patrons['data']:
             f.write(patron['attributes']['full_name'] + ":" + get_tier_from_id(patron['relationships']['currently_entitled_tiers']['data'][0]['id']))
     print("Done!")
